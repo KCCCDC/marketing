b1159b8cdda44dd547a6d10516dc4fbc2b4e0b6b
Keane O'Kelley <keane.m.okelley@gmail.com>
2016-02-08 00:39:33 -0600

    Test for pumps

--- opc_get_values.py
+++ opc_get_values.py
@@ -10,11 +10,13 @@ relay1=paths['relay1']
 relay2=paths['relay2']
 gen1=paths['gen1']
 gen2=paths['gen2']
+pump0=paths['pump0']
 
 xda_relay1 = XDAClient(OPCServerAddress=relay1,ReturnErrorText=True)
 xda_relay2 = XDAClient(OPCServerAddress=relay2,ReturnErrorText=True)
 xda_gen1 = XDAClient(OPCServerAddress=gen1,ReturnErrorText=True)
 xda_gen2 = XDAClient(OPCServerAddress=gen2,ReturnErrorText=True)
+xda_water = XDAClient(OPCServerAddress=water,ReturnErrorText=True)
 
 
 print xda_relay1.Read([ItemContainer(ItemName='relay1_breaker')])[0][0].Value
@@ -29,3 +31,32 @@ print xda_gen1.Read([ItemContainer(ItemName='gen1_generation')])[0][0].Value
 print xda_gen2.Read([ItemContainer(ItemName='gen2_breaker')])[0][0].Value
 print xda_gen2.Read([ItemContainer(ItemName='gen2_generation')])[0][0].Value
 
+print xda_pump0.Read([ItemContainer(ItemName='PS0_PUMP_0')])[0][0].Value
+print xda_pump0.Read([ItemContainer(ItemName='PS0_PUMP_1')])[0][0].Value
+print xda_pump0.Read([ItemContainer(ItemName='PS0_VALVE_0')])[0][0].Value
+print xda_pump0.Read([ItemContainer(ItemName='PS0_VALVE_1')])[0][0].Value
+
+print xda_pump1.Read([ItemContainer(ItemName='PS1_PUMP_0')])[0][0].Value
+print xda_pump1.Read([ItemContainer(ItemName='PS1_PUMP_1')])[0][0].Value
+print xda_pump1.Read([ItemContainer(ItemName='PS1_VALVE_0')])[0][0].Value
+print xda_pump1.Read([ItemContainer(ItemName='PS1_VALVE_1')])[0][0].Value
+
+print xda_pump2.Read([ItemContainer(ItemName='PS2_PUMP_0')])[0][0].Value
+print xda_pump2.Read([ItemContainer(ItemName='PS2_PUMP_1')])[0][0].Value
+print xda_pump2.Read([ItemContainer(ItemName='PS2_VALVE_0')])[0][0].Value
+print xda_pump2.Read([ItemContainer(ItemName='PS2_VALVE_1')])[0][0].Value
+
+print xda_pump3.Read([ItemContainer(ItemName='PS3_PUMP_0')])[0][0].Value
+print xda_pump3.Read([ItemContainer(ItemName='PS3_PUMP_1')])[0][0].Value
+print xda_pump3.Read([ItemContainer(ItemName='PS3_VALVE_0')])[0][0].Value
+print xda_pump3.Read([ItemContainer(ItemName='PS3_VALVE_1')])[0][0].Value
+
+print xda_pump4.Read([ItemContainer(ItemName='PS4_PUMP_0')])[0][0].Value
+print xda_pump4.Read([ItemContainer(ItemName='PS4_PUMP_1')])[0][0].Value
+print xda_pump4.Read([ItemContainer(ItemName='PS4_VALVE_0')])[0][0].Value
+print xda_pump4.Read([ItemContainer(ItemName='PS4_VALVE_1')])[0][0].Value
+
+print xda_pump5.Read([ItemContainer(ItemName='PS5_PUMP_0')])[0][0].Value
+print xda_pump5.Read([ItemContainer(ItemName='PS5_PUMP_1')])[0][0].Value
+print xda_pump5.Read([ItemContainer(ItemName='PS5_VALVE_0')])[0][0].Value
+print xda_pump5.Read([ItemContainer(ItemName='PS5_VALVE_1')])[0][0].Value
